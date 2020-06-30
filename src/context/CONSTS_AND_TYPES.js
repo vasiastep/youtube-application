731b18b5a092e3995bad89083b6d295d5f0e7db0
Vasyl Stepanov <58212703+vasiastep@users.noreply.github.com>
2020-06-30 14:23:13 +0300

    New api key

--- src/context/CONSTS_AND_TYPES.js
+++ src/context/CONSTS_AND_TYPES.js
@@ -1,5 +1,6 @@
 export const BASE_URL = 'https://www.googleapis.com/youtube/v3'
-export const API_KEY = 'AIzaSyBkV9ea_vdXr0BtngYLGvmvHJYolWHlI9Y'
+// export const API_KEY = 'AIzaSyBkV9ea_vdXr0BtngYLGvmvHJYolWHlI9Y'
+export const API_KEY = 'AIzaSyDTECwDVR4BV17M8cD7Ci6ulMk3Cg6QuRI'
 export const TOGGLE_MENU = 'TOGGLE_MENU'
 export const FETCH_VIDEOS = 'FETCH_VIDEOS'
 export const SELECT_VIDEO = 'SELECT_VIDEO'
